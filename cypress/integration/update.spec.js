d7d008d2b6a3d9a3808e0c6669459ebfc3e02a65
Liang Ding <d@b3log.org>
2021-08-02 07:24:25 +0800

    :art: 引入 Cypress

--- cypress/integration/update.spec.js
+++ cypress/integration/update.spec.js
@@ -1,6 +1,22 @@
-// update.spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
+describe('example to-do app', () => {
+  before(() => {
+    cy.visit('http://127.0.0.1:6806')
+  })
+
+  it("open help", () => {
+    cy.get("#barMore").click()
+    cy.get("#commonMenu").find(".b3-menu__item").last().click()
+    cy.wait(5000)
+  })
+
+  it("fold heading", () => {
+    cy.get('.protyle-breadcrumb__item[data-node-id="20200812220555-lj3enxa"]').click()
+
+    cy.get('[data-node-id="20210428212840-8rqwn5o"]')
+    cy.get('[data-node-id="20210428212840-859h45j"][data-type="NodeHeading"]').first().type("{ctrl+uparrow}")
+    cy.get('[data-node-id="20210428212840-8rqwn5o"]').should("not.exist")
+
+    cy.get('[data-node-id="20210428212840-859h45j"][data-type="NodeHeading"]').first().type("{ctrl+uparrow}")
+    cy.get('[data-node-id="20210428212840-8rqwn5o"]').should("exist")
+  })
+})
